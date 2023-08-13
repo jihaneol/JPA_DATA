a4666dbfd7ae475e972e38c72c9b15514ce3dd60
ji <6531z@naver.com>
2023-08-13 18:19:40 +0900

    feature: NamedQuery

---
+++ jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    private  MemberRepository memberRepository;
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        assertEquals(result.get(0).getUsername(),"AAA");
+        assertEquals(result.get(0).getAge(), 20);
+
+
+    }
+}
